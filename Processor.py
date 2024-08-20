0a98ae318b710de313402dce920cd80cdfd74b13
Kalle <kalle.t.sipila@gmail.com>
2024-08-20 12:47:02 +0800

    Spacing for readability

--- Processor.py
+++ Processor.py
@@ -14,16 +14,19 @@ class Paths(Enum):
 
 class Processor:
   def __init__(self) -> None:
+
     self._file_path = ""
     self._folder_path = ""
     self._xl_path = ""
     self._out_path = ""
 
   def set_path(self, file_path: str):
+
     self._file_path = file_path
     self._folder_path = os.path.dirname(file_path)
     self._xl_path = self._folder_path + r"\Translate.xlsx"
     self._out_path = file_path.replace(".pptx", "_OUT.pptx")
+
     return self
   
   def get_path(self, path:int):
@@ -39,11 +42,14 @@ class Processor:
   def extract_text(self):
     # Open the .pptx file as a zip archive
     z = zipfile.ZipFile(self._file_path, "r")
+
     # Get a list of all slide files
     slide_files = [name for name in z.namelist() if "ppt/slides/slide" in name and not "-rels" in name]
+
     # Create a new Excel workbook
     wb = openpyxl.Workbook()
     ws = wb.active
+
     # Loop over each slide
     for slide_file in slide_files:
       # Read the XML data
@@ -53,11 +59,14 @@ class Processor:
       # Define the namespaces
       namespaces = {"a": "http://schemas.openxmlformats.org/drawingml/2006/main",
                     "p": "http://schemas.openxmlformats.org/presentationml/2006/main"}
+      
       # Loop over each text elem
       for elem in root.findall(".//a:t", namespaces):
         ws.append([elem.text])
       wb.save(self._xl_path)
+
     z.close()
+
     return self
 
   def google_translate(self, source_lang: str = "en", to_lang: str = "zh-CN", css_selector: str = ".ryNqvb",
@@ -70,8 +79,10 @@ class Processor:
     wb = openpyxl.load_workbook(file_path)
     sheet = wb.active
     result = ""
+
     for row in sheet.iter_rows(min_col=1, max_col=1):
       result += str(row[0].value) + "\n"
+
     with sync_playwright() as p:
       browser = p.chromium.launch(headless=True, executable_path=chrome_path)
       page = browser.new_page()
@@ -81,12 +92,15 @@ class Processor:
       page.wait_for_selector(css_selector)
       spans = page.query_selector_all(css_selector)
       translation = ""
+
       for span in spans:
         translation += span.inner_text()
       browser.close()
       rows = translation.split("\n")
+
       for i, row in enumerate(rows, start=1):
         sheet.cell(row=i, column=2, value=row)
+
       wb.save(file_path)
 
     
@@ -94,12 +108,16 @@ class Processor:
     zin = zipfile.ZipFile(self._file_path, "r")
     zout = zipfile.ZipFile(self._out_path, "w")
     slide_files = [name for name in zin.namelist() if "ppt/slides/slide" in name and not "-rels" in name]
+
     for item in zin.infolist():
       if item.filename not in slide_files:
         zout.writestr(item, zin.read(item.filename))
+
     wb = openpyxl.load_workbook(self._xl_path)
     ws = wb.active
+
     translations = [row[1].value for row in ws.iter_rows()]
+
     # Loop over each file
     for slide_file in slide_files:
       slide_xml = zin.read(slide_file)
@@ -107,15 +125,16 @@ class Processor:
       root = ET.fromstring(slide_xml)
       namespaces = {"a": "http://schemas.openxmlformats.org/drawingml/2006/main",
                     "p": "http://schemas.openxmlformats.org/presentationml/2006/main"}
+      
       for i, elem in enumerate(root.findall(".//a:t", namespaces)):
         if translations[i] == None:
           elem.text = ""
         else:
           elem.text = str(translations[i])
+
       # Write modified xml back to slide
       slide_xml_new = ET.tostring(root).decode()
       zout.writestr(slide_file, slide_xml_new)
+
     zin.close()
     zout.close()
-      
-
--- main.py
+++ main.py
@@ -10,18 +10,23 @@ def main():
   
   parser.add_argument("task", choices=["extract", "merge"], type=str,
                       help="`extract` to extract text for translation, `merge` to replace text in PowerPoint")
+  
   parser.add_argument("path", metavar=r"C:\PowerPoint\Path.pptx", type=str,
                       help="Specify full path of your .pptx file")
+  
   parser.add_argument("-a", "--auto", required=False, action="store_true",
                       help="Auto translate by Google Translate using Playwright")
+  
   parser.add_argument("-s", "--source", required=False, default="en", type=str,
                       help="From language - source language. Defaults to `en` for English")
+  
   parser.add_argument("-t", "--to", required=False, default="zh-CN", type=str,
                       help="To language - destination language. Defaults to `zh-CN` for simplified Chinese")
 
   args = parser.parse_args()
   ppt_path = args.path
   task = args.task
+  
   if task == "extract":
     if args.auto == True:
       Processor().set_path(ppt_path).extract_text().google_translate(source_lang=args.source, to_lang=args.to)
